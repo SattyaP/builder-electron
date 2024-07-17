43eee318e053104ea59046c7b4a081a2bb74b292
SattyaP <stylearning7@gmail.com>
2024-07-17 10:47:03 +0700

    chore(dep): add dependencies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 const JavaScriptObfuscator = require("javascript-obfuscator");
 const cliProgress = require("cli-progress");
 
